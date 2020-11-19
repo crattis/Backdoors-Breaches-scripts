d5cd07a2d368ff1f61d96411494c282b2e8380bd
Chris J <crattis@users.noreply.github.com>
2020-11-19 02:33:00 -0500

    fixed flake8 errors about line being too long.

--- bb_scenario_generator.py
+++ bb_scenario_generator.py
@@ -17,8 +17,10 @@ def procedure_deck():
 
 def main():
     print("Break the deck in to its six colored parts. Shuffle each sub-deck.")
-    print(f'Select card number {select_deck(10)} from the Initial Compromise deck.')
-    print(f'Select card number {select_deck(7)} from the Pivot and Escalate deck.')
+    print(f'Select card number {select_deck(10)} from the Initial Compromise '
+          ' deck.')
+    print(f'Select card number {select_deck(7)} from the Pivot and Escalate '
+          'deck.')
     print(f'Select card number {select_deck(9)} from the Persistence deck.')
     print(f'Select card number {select_deck(6)} from the C2 and Exfil deck.')
     procedure_deck()
